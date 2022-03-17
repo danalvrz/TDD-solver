27f0c848537577b4d13c4ccd87eeda645c567a24
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-03-17 14:53:32 -0600

    Add code to pass test and more tests

--- solver.rb
+++ solver.rb
@@ -1,5 +1,13 @@
 class Solver
-  def factorial(number); end
+  def factorial(number)
+    x = 1
+    result = 1
+    while x < number
+        x += 1
+        result = result * x
+    end
+    result
+  end
 
   def reverser(word); end
 
--- spec/solver_rspec.rb
+++ spec/solver_rspec.rb
@@ -8,7 +8,17 @@ describe 'Solver' do
 
   describe 'factorial method' do
     it 'takes one number as argument and returns the factorial for that number' do
-      expect(@solver.factorial(5)).to eq 120
+      expect(@solver.factorial(10)).to eq 3628800
+    end
+
+    it 'returns 1 if N is equal to zero' do
+        expect(@solver.factorial(0)).to eq 1
+    end
+
+    it 'raises en error if N < 0' do
+        number = rand(1..10) * -1
+        expect(@solver.factorial(number)). to raise_error('The factorials of negative integers cannot be computed!')
     end
   end
+
 end

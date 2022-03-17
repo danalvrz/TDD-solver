5ca322d601be34daf18ded722550682b4a0b1938
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-03-17 14:22:17 -0600

    Create files and add first test

---
+++ solver.rb
@@ -0,0 +1,11 @@
+class Solver
+    def factorial(n)
+    end
+
+    def reverser(word)
+    end
+
+    def fizz_buzz(n)
+    end
+    
+end
---
+++ spec/solver_rspec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require './solver'
+
+describe "Solver" do
+    before :each do
+        @solver = Solver.new()
+    end
+
+    describe "factorial method" do
+
+        it "takes one number as argument and returns the factorial for that number" do
+            expect(@solver.factorial(5)).to eq 120
+        end
+    end
+end

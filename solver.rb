100ff1b434d2e31850781eb89495e626eaf04199
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-03-17 15:30:36 -0600

    Add test for fizz_buzz

--- solver.rb
+++ solver.rb
@@ -3,16 +3,17 @@ class Solver
     x = 1
     result = 1
     return 'The factorials of negative integers cannot be computed!' if number.negative?
+
     while x < number
-        x += 1
-        result = result * x
+      x += 1
+      result *= x
     end
     result
   end
 
-  def reverser(word); 
-  word.reverse
+  def reverser(word)
+    word.reverse
   end
 
-  def fizz_buzz(nnumber); end
+  def fizz_buzz(number); end
 end
--- spec/solver_rspec.rb
+++ spec/solver_rspec.rb
@@ -8,16 +8,16 @@ describe 'Solver' do
 
   describe 'factorial method' do
     it 'takes one number as argument and returns the factorial for that number' do
-      expect(@solver.factorial(10)).to eq 3628800
+      expect(@solver.factorial(10)).to eq 3_628_800
     end
 
     it 'returns 1 if N is equal to zero' do
-        expect(@solver.factorial(0)).to eq 1
+      expect(@solver.factorial(0)).to eq 1
     end
 
     it 'raises en error if N < 0' do
-        number = rand(1..10) * -1
-        expect(@solver.factorial(number)).to eq 'The factorials of negative integers cannot be computed!'
+      number = rand(1..10) * -1
+      expect(@solver.factorial(number)).to eq 'The factorials of negative integers cannot be computed!'
     end
   end
 
@@ -27,4 +27,9 @@ describe 'Solver' do
     end
   end
 
+  describe 'fizz_buzz method' do
+    it 'returns fizz if number is divisible by 3' do
+      expect(@solver.fizz_buzz(6)).to eq 'fizz'
+    end
+  end
 end
